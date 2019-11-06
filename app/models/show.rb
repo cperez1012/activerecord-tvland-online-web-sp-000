d2102cead0775eb1c14ae347c0ad7b2ffd83febf
Christian Perez <perezchristian1012@gmail.com>
2019-11-06 21:51:27 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,9 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
+  def actors_list
+    self.characters.collect do |character|
+      "#{character.actor.first_name} #{character.actor.last_name}"
+    end
+  end
 end
